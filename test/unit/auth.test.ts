3df0b37c622f987cb67e481bc9df96634c43889a
Robson Wendel <robson.costa134@gmail.com>
2024-05-17 10:57:39 -0300

    feat: novos testes de autenticação implementados

--- test/unit/auth.test.ts
+++ test/unit/auth.test.ts
@@ -2,6 +2,8 @@ import { JwtService } from '@nestjs/jwt'
 import { InMemoryAuthService } from '../../src/services/in-memory/in-memory-auth'
 import { InMemoryUsersService } from '../../src/services/in-memory/in-memory-users'
 import { describe, it, expect } from 'vitest'
+import { EmailAlreadyExistException } from '../../src/http/errors/email-already-exit'
+import { EmailNotRegisteredException } from '../../src/http/errors/email-not-registered'
 
 const inMemoryAuthService = new InMemoryAuthService(
   new InMemoryUsersService(),
@@ -37,5 +39,33 @@ describe('Authentication Tests', () => {
     )
 
     expect(accessToken).toEqual(expect.any(String))
+  })
+
+  it('It should not be possible to register a user with an registered email', async () => {
+    const data = {
+      username: 'test',
+      email: 'test@gmail.com',
+      password: '123456',
+    }
+
+    expect(
+      async () =>
+        await inMemoryAuthService.signUp(
+          data.username,
+          data.email,
+          data.password,
+        ),
+    ).rejects.toBeInstanceOf(EmailAlreadyExistException)
+  })
+
+  it('It should not be possible to log in to a user with a non-existent email address', () => {
+    const data = {
+      email: 'notregisteredemail@gmail.com',
+      password: '123456',
+    }
+
+    expect(
+      async () => await inMemoryAuthService.signIn(data.email, data.password),
+    ).rejects.toBeInstanceOf(EmailNotRegisteredException)
   })
 })
