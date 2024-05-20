18c35a2fe9b060c3470b59c22b29de375e78b9e7
Robson Wendel <robson.costa134@gmail.com>
2024-05-20 09:33:50 -0300

    feat: testes de posts implementados

--- test/unit/posts.test.ts
+++ test/unit/posts.test.ts
@@ -1,6 +1,7 @@
 import { randomUUID } from "crypto";
 import { InMemoryPostsService } from "../../src/services/in-memory/in-memory-posts";
 import { describe, it, expect } from "vitest";
+import { Post } from "@prisma/client";
 
 const inMemoryPostsService = new InMemoryPostsService()
 
@@ -25,6 +26,11 @@ describe('Posts Service Testing', () => {
     expect(post.id).toEqual(expect.any(String))
   })
 
+  it('its must possible to list all posts', async () => {
+    const post = await inMemoryPostsService.list('testingUserId')
+    expect(post).toBeInstanceOf(Array<Post>)
+  })
+
   it('its must possible to find post by id', async () => {
 
     const findPostData = {
@@ -34,5 +40,16 @@ describe('Posts Service Testing', () => {
 
     const post = await inMemoryPostsService.findPostById(findPostData.id, findPostData.userId)
     expect(post?.id).toEqual(findPostData.id)
+  })
+
+  it('its must possible to delet post by id', async () => {
+
+    const findPostData = {
+      id: 'testingId',
+      userId: 'testingUserId'
+    }
+
+    const post = await inMemoryPostsService.deletePost(findPostData.id, findPostData.userId)
+    expect(post?.id).toEqual(undefined)
   })
 })
